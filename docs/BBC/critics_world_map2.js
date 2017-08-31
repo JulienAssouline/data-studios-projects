dda00c51cdebf9520a1aff2f1a329764af700767
Julien Assouline <julienassouline@dyn-160-39-196-41.dyn.columbia.edu>
2017-08-31 12:14:00 -0400

    BBC_update2

--- docs/BBC/critics_world_map2.js
+++ docs/BBC/critics_world_map2.js
@@ -42,7 +42,7 @@
 
 
         d3.json("world.topojson", function(json){
-          d3.csv("Critics by Country 4.csv", function(error, data){
+          d3.csv("Critics_by_Country_4.csv", function(error, data){
 
 
 

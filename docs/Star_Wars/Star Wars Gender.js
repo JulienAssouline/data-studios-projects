4ade0097c9e21c895556092b8dc3316680c88248
Julien Assouline <julienassouline@dyn-160-39-196-41.dyn.columbia.edu>
2017-10-12 22:09:03 -0400

    stroke_change

--- docs/Star_Wars/Star Wars Gender.js
+++ docs/Star_Wars/Star Wars Gender.js
@@ -185,16 +185,16 @@
                 .duration(200)
                 .style("opacity", 1)
                 var element = d3.select(this);
-                element.style("stroke", "black")
-                element.style("stroke-width", 2)
+                element.style("stroke", "darkgrey")
+                element.style("stroke-width", 1)
                 div.html(d.Characters + ": " + d.Word_count+" words")
                 div.style("visibility", "visible")
                 .style("left", (d3.event.pageX - 20) + "px")    
-                .style("top", (d3.event.pageY - 35) + "px")
+                .style("top", (d3.event.pageY - 45) + "px")
               })
               .on("mousemove", function(d){
             div.style("left", (d3.event.pageX - 20) + "px")    
-            .style("top", (d3.event.pageY - 35) + "px")
+            .style("top", (d3.event.pageY - 45) + "px")
           })
               .on("mouseout", function(d){
                 // div.transition()

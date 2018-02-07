af7de5fc014fb05466a087c91a66181cc8a10822
JulienAssouline <julien1993@hotmail.ca>
2018-02-06 22:25:04 -0500

    North_Korea_update

--- docs/North_Korea/North_Korea_imports.js
+++ docs/North_Korea/North_Korea_imports.js
@@ -2,8 +2,7 @@
 
      var w = 900;
     var h = 500;
-var lowColor = '#f9f9f9'
-var highColor = '#bc2a66'
+
 
       var margin = {
           top: 10,
@@ -39,18 +38,31 @@ var highColor = '#bc2a66'
 
         var purple = ["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]
 
-        var color = d3.scaleThreshold()
+        var color_imp = d3.scaleThreshold()
             .domain([0, 1688, 206684, 747204.800, 3652593.657, 1000000000, 2338271127])
             .range(["#ede9f3", "#cbbedd", "#a993c7", "#8767b0", "#653c9a", "#54278f", "#431f72"]);
 
-            var countryMap = d3.map();
+
+        var color_exp = d3.scaleThreshold()
+            .domain([0, 1688, 206684, 747204.800, 3652593.657, 1000000000, 2338271127])
+            .range(["#ede9f3", "#cbbedd", "#a993c7", "#8767b0", "#653c9a", "#54278f", "darkgreen"]);
+
+            var countryMap_imp = d3.map();
+            var countryMap_exp = d3.map();
 
         d3.queue()
           .defer(d3.json, "readme-world-110m.json")
           .defer(d3.csv, "North_Korea_imports_2015.csv", function(d){
             d.import_val = +d.import_val
 
-            countryMap.set(d.name, d)
+            countryMap_imp.set(d.name, d)
+
+            return d
+          })
+          .defer(d3.csv, "df_NK_merged_geo_2015.csv", function(d){
+            d.export_val = +d.export_val
+
+            countryMap_exp.set(d.name, d)
 
             return d
           })
@@ -63,12 +75,46 @@ var highColor = '#bc2a66'
 
           console.log(countries)
 
-          svg.append("text")
-            .attr("x", 0)
+          var text = svg.append("svg:text")
+           
+            text.append("svg:tspan")
             .attr("y", 10)
-            .text("Countries exporting to North Korea, 2015")
+            .style("fill", "black")
+            .text("North Korea ")
+            .style("font-size", 20)
+            .style("font-weight", "bold");
+
+             text.append("svg:tspan")
+            .attr("y", 10)
+            .attr("fill", "#431f72")
+            .text("Imports")
+            .style("text-decoration", "underline")  
             .style("font-size", 20)
             .style("font-weight", "bold")
+            .transition()
+            .delay(500)
+            .duration(500)
+            .on("start", function repeat(){
+              d3.active(this)
+              .transition()
+              .delay(3000)
+              .duration(2000)
+              .attr("fill", "darkgreen")
+              .text("Exports")
+              .transition()
+              .delay(3000)
+              .duration(2000)
+              .attr("fill", "#431f72")
+              .text("Imports")
+              .on("end", repeat)
+            });
+
+            text.append("svg:tspan")
+            .attr("y", 10)
+            .style("fill", "black")
+            .text(", 2015")
+            .style("font-size", 20)
+            .style("font-weight", "bold");
 
         svg.selectAll("path")
           .data(countries)
@@ -78,14 +124,49 @@ var highColor = '#bc2a66'
           .attr("d", path)
           .style("stroke", "white")
           .style("fill", function(d){
-            var countryData = countryMap.get(d.id)
-            if(countryData){
-              return color(countryData)// here i just want one color
+            var countryData_imp = countryMap_imp.get(d.id)
+            if(countryData_imp){
+              return color_imp(countryData_imp)// here i just want one color
               // return color(countryData.import_val) this is how I would add the choropleth
             } else {
               return "#ccc"
             }
           })
+          .transition()
+            .delay(500)
+            .duration(500)
+            .on("start", function repeat(){
+              d3.active(this)
+                .transition()
+                .delay(3000)
+                .duration(2000)
+                .style("fill", function(d){
+                   var countryData_exp = countryMap_exp.get(d.id)
+                  if(countryData_exp){
+                    return color_exp(countryData_exp)// here i just want one color
+              // return color(countryData.import_val) this is how I would add the choropleth
+                    } else {
+                    return "#ccc"
+                    }
+                    })
+                .transition()
+                .delay(3000)
+                .duration(2000)
+                .style("fill", function(d){
+                   var countryData_imp = countryMap_imp.get(d.id)
+                  if(countryData_imp){
+                    return color_imp(countryData_imp)// here i just want one color
+              // return color(countryData.import_val) this is how I would add the choropleth
+                    } else {
+                    return "#ccc"
+                    }
+                    })
+                .on("end", repeat)
+
+                }) // repeat
+
+
+    
 
 
 
